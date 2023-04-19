d33e56d965c4692997f2cff869956f369fa1904a
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-04-19 18:51:58 +0200

    add methods to render and add tasks

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,18 +1,110 @@
 import React from 'react';
+import API from './API';
 
 class TasksManager extends React.Component {
-    state = {
-        tasks: [],
-    }
+	constructor() {
+		super();
+		this.api = new API();
+	}
 
-    onClick = () => {
-        const { tasks } = this.state;
-        console.log( tasks)
-    }
+	state = {
+		tasks: [],
+		task: '',
+	};
 
-    render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
-    }
+	componentDidMount() {
+		this.api.loadData().then((data) => {
+			this.setState({
+				tasks: data,
+			});
+		});
+	}
+
+	render() {
+		return (
+			<>
+				<h1 onClick={this.onClick}>TasksManager</h1>
+				<section>
+					<form onSubmit={this.submitHandler}>
+						<label>Nazwa: </label>
+						<input
+							name='task'
+							value={this.state.task}
+							onChange={this.inputChange}
+						/>
+
+						<input type='submit' />
+					</form>
+					<ul> {this.renderTasksList()}</ul>
+				</section>
+			</>
+		);
+	}
+
+	onClick = () => {
+		const { tasks } = this.state;
+		console.log(tasks);
+	};
+
+	inputChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value,
+		});
+	};
+
+	renderTasksList() {
+		const { tasks } = this.state;
+		return tasks.map((task) => {
+			return (
+				<section>
+					<header>{task.name}, 00:00:00</header>
+					<footer>
+						<button>start/stop</button>
+						<button>zakończone</button>
+						<button disabled='true'>usuń</button>
+					</footer>
+				</section>
+			);
+		});
+	}
+
+	submitHandler = (e) => {
+		e.preventDefault();
+
+		const { task } = this.state;
+		if (task) {
+			this.addTask(task);
+		} else {
+			alert('Wprowadź treść zadania!');
+		}
+	};
+
+	addTask(taskName) {
+		const data = {
+			name: taskName,
+			time: 0,
+			isRunning: false,
+			isDone: false,
+			isRemoved: false,
+		};
+
+		this.api.addData(data).then((resp) => {
+			if (resp.id) {
+				this.addTaskToState(resp);
+			}
+		});
+	}
+
+	addTaskToState(task) {
+		const newItem = task;
+
+		this.setState((state) => {
+			return {
+				tasks: [...state.tasks, newItem],
+			};
+		});
+	}
 }
 
 export default TasksManager;
