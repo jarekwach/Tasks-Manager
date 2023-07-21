27bd240fc00c8d214481a9b37e5db1800dff3968
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-07-21 14:26:49 +0200

    remove unused code

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,30 @@
 const path = require('path');
-// importuję bibliotękę [path] z [node.js]
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// importuję odpowiedni plugin
+
 module.exports = {
-    entry: './src/app.js',
-    // definiuje plik wejściowy
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        // definiuje ścieżką wyjściową
-        filename: 'app.min.js',
-        // definiuję nazwę pliku wyjściowego
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                // określam jakie pliki 
-                // będą brane pod uwagę
-                exclude: /node_modules/,
-                // określam wykluczenia
-                use: 'babel-loader',
-                // określam jaki [loader]
-                // ma być wykorzystany
-            },
-            {
-                test: /\.css$/i,
-                // wszystkie pliki, których nazwa
-                // kończy się na .css
-                use: [
-                    'style-loader',
-                    // dodaj odczytaną zawartość
-                    // do znacznika <style/>
-                    'css-loader', 
-                    // odczytaj plik CSS
-                ],
-            }
-        ]
-        // obecnie brak dodatkowych ustawień
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            // wskazuję plik źródłowy
-            filename: 'index.html'
-            // określam nazwę dla pliku
-        })
-    ]
-}
-// eksportuję ustawienia dla webpack-a
+	entry: './src/app.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'app.min.js',
+	},
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+		}),
+	],
+};
